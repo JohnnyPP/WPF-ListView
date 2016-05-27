c251cc9fa4708c904d51ce9769ad571bc6a9229a
Johnny P <johnnnyppp@gmail.com>
2016-05-27 17:20:32 +0200

    Initial release

--- ListView/MainWindow.xaml.cs
+++ ListView/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,27 @@ namespace ListView
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public List<Person> Guys { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = this;
+
+            Guys = new List<Person>();
+            Guys.Add(new Person("Julius Caesar", 40));
+            Guys.Add(new Person("Pompeius Magnus", 46));
+            Guys.Add(new Person("Marcus Crassus", 55));
+            RaisePropertyChanged("Guys");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
         }
     }
 }
