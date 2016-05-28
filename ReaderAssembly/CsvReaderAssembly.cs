7e62355dd2bbd3a4f24b80c89db4b5e367ef2015
Johnny P <johnnnyppp@gmail.com>
2016-05-28 16:51:30 +0200

    Added new assembly

---
+++ ReaderAssembly/CsvReaderAssembly.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaderAssembly
+{
+    public class CsvReaderAssembly : INotifyPropertyChanged
+    {
+        public List<ReaderDataAssembly> ReaderListDataAssembly { get; set; }
+
+        public CsvReaderAssembly()
+        {
+            ReaderListDataAssembly = new List<ReaderDataAssembly>();
+            ReaderListDataAssembly.Add(new ReaderDataAssembly("Julius Caesar 3", 60));
+            ReaderListDataAssembly.Add(new ReaderDataAssembly("Pompeius Magnus 3", 66));
+            ReaderListDataAssembly.Add(new ReaderDataAssembly("Marcus Crassus 3", 75));
+            OnRaisePropertyChanged("ReaderListDataAssembly");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnRaisePropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
+
---
+++ ReaderAssembly/ReaderDataAssembly.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaderAssembly
+{
+    public class ReaderDataAssembly
+    {
+        public string Name3 { get; set; }
+        public int Age3 { get; set; }
+
+        public ReaderDataAssembly(string name, int age)
+        {
+            Name3 = name;
+            Age3 = age;
+        }
+    }
+}
